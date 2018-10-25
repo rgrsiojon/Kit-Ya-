dacc17088b5be1f7d6224a811cf674c94abc94df
rgrsiojon <43159394+rgrsiojon@users.noreply.github.com>
2018-10-25 13:22:47 +0700

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port =  process.env.PORT || 3000;
 
 const controller =  require(__dirname + '/apps/controllers');
 
 app.use(controller);
 
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log('server runnong port 3000');
 })
